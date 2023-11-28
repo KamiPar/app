2ba706dbea7b0e0d0b3cce51ed2c07651d36fa70
Kamil <Partyka.K98@gmail.com>
2023-11-28 22:18:47 +0100

    First frontend application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {WeatherComponent} from "./weather/weather.component";
+import {MainComponent} from "./main/main.component";
+import {TodoComponent} from "./todo/todo.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'weather', component: WeatherComponent},
+  {path: '', component: MainComponent},
+  {path: 'todo-list', component: TodoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatListModule} from "@angular/material/list";
+import { WeatherComponent } from './weather/weather.component';
+import { MainComponent } from './main/main.component';
+import { TodoComponent } from './todo/todo.component';
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    MainComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
